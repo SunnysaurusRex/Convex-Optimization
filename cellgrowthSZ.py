3d173dca18f71478392233179ac1777da29d7d5a
SunE <sunnyzhao1122@gmail.com>
2022-03-19 19:29:46 -0400

    test commit
    test commit
    
    working on ODE solution using ODE solver, np.optimize

--- cellgrowthSZ.py
+++ cellgrowthSZ.py
@@ -16,7 +16,7 @@ d_n = 2.3 		# the exponent in the equation
 # GLN glutamine differential equation
 K_d_gln = 9e-3	# constant for glutamine degradation [=] 1/h
 Y_x_gln = 4e8 	# yield of cells on glutamine [=] cells/mmol
-Y_gln_glu = 1 	# yield of glutamine from glucose [=] mmol/mmol
+Y_gln_glu = 1 	# yield of glutamine from glutamate [=] mmol/mmol
 a1 = 3.4e-13	# alpha_1 of glutamine maintenance coefficient [=] mM/L/cell/h
 a2 = 4 			# alpha_2 " " " " [=] mM
 # GLC glucose differential equation
@@ -26,3 +26,36 @@ M_glc = 4.8e-14	# maintenance coefficient for glucose [=] mmol/cell/h
 Y_amm_gln = 9e-1 # yield of ammonia on glutamine [=] mmol/mmol
 # X_d dead cells differential equation
 K_lysis = 1.3e-2 # Monod constant for lyssis [=] mM
+
+# initial conditions
+X0 = 3e5		# inoculum [=] cells/mL ; assume 100% viability <=> X_v
+Xd0 = 0 		# no dead cells at the beginning
+GLC_0 = 55		# glucose concentration [=] mM
+GLN_0 = 4 		# glutamine concentration [=] mM
+AMM_0 = 1e-12	# ammonia concentration [=] mM 
+
+t = 100 		# solve ODEs up to this time in hours
+
+IC = np.array([X0, Xd0, GLC_0, GLN_0, AMM_0])
+
+# ODE function
+def HEK293(t, w): # inputs: time value, solution vector at current iteration
+	# decompose w vector
+	Xv = w[0]
+	Xd = w[1]
+	GLC = w[2]
+	GLN = w[3]
+	AMM = w[4]
+	# specfic growth and death rates
+	mu = mu_min + (mu_max-mu_min)*GLCU*GLN/(K_glc+GLC)/(K_gln+GLN)
+	mu_d = mu_d_max/(1+(K_d_amm/AMM)**d_n)
+	# intermediate variables for ODEs
+	M_gln = a1*GLN/(a2+GLN)
+	Q_gln = -mu/Y_x_gln - M_gln
+	Q_glc = -mu/Y_x_glc - M_glc
+	Q_amm = -Y_amm_gln*Q_gln
+	# ODE
+	dXv = (mu-mu_d)*Xv
+	dXd = (mu_d-K_lysis)*Xd
+	dGLN =  
+
